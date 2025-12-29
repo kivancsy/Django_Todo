767cc5d0934dbc6c37dbe5ca0826c4a008bd61f3
kivancsy <kivancs.yildirim@gmail.com>
2025-12-29 13:56:35 +0300

    update task added

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path, include
-
 from todo import views
 
+
 urlpatterns =[
+    
+    #Add task
     path('add-task/', views.add_task, name = 'add_task'),
+
+    #MARK as done/undone
     path('mark-as-done/<int:pk>/', views.mark_as_done, name = 'mark_as_done'),
-    path('mark-as-undone/<int:pk>', views.mark_as_undone, name = 'mark_as_undone')
+    path('mark-as-undone/<int:pk>', views.mark_as_undone, name = 'mark_as_undone'),
+    
+    #EDIT Feature
+    path('edit_task/<int:pk>/', views.edit_task, name='edit_task'),    
 ]
